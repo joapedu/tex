23f3453499bc55cd4401b87065dad4849eb8ceab
joapedu <joaoeduardobraga2@gmail.com>
2024-08-12 03:20:31 -0300

    raw: execução das procedures

---
+++ scripts/execucoes/exec_atualizar_pessoa.sql
@@ -0,0 +1,10 @@
+SELECT atualizar_pessoa(
+    1,
+    'João Eduardo Atualizado',
+    '2003-05-12',
+    3500.00,
+    'Novas observações',
+    'Pollyana Braga',
+    'Anderson Cruz',
+    '12345678901'
+);
---
+++ scripts/execucoes/exec_inserir_pessoa.sql
@@ -0,0 +1,9 @@
+SELECT inserir_pessoa(
+    'João Eduardo',
+    '2003-05-12',
+    3000.00,
+    'Observações aqui',
+    'Pollyana Braga',
+    'Anderson Cruz',
+    '12345678901'
+);
