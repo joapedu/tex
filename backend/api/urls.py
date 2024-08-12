d064eec4ee7a017ba7235e93523eca69f0114067
joapedu <joaoeduardobraga2@gmail.com>
2024-08-12 12:42:52 -0300

    ci: ajustes para workflow work well

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,4 @@
+"""Arquivo de urls da api."""
 from django.urls import path
 from .views import PessoasApiView
 
--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,14 +1,1 @@
-import psycopg2
-from django.db import connection, IntegrityError
-
-def validar_entrada_dados(self, data, required_fields_with_types):
-    """Método de apoio para validação de dados."""
-    errors = {}
-
-    for field, field_type in required_fields_with_types.items():
-        if field not in data or data[field] in [None, '']:
-            errors[field] = f"{field} é obrigatório."
-        elif not isinstance(data[field], field_type):
-            errors[field] = f"{field} deve ser do tipo {field_type.__name__}."
-
-    return errors
+""" Arquivo de funções úteis do sistema."""
